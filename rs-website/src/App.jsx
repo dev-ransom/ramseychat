875ac1e050a119e52af3b6d695f81ffbfa552ea1
dev-ransom <sundayyoung64@gmail.com>
2024-06-04 14:46:44 +0100

    added code for wrap up of project

--- rs-website/src/App.jsx
+++ rs-website/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Background from './components/Background/Background'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
@@ -8,9 +8,17 @@ const App = () => {
     { text1: 'Dive into', text2: 'what you love' },
     { text1: 'Indulge', text2: 'Your passions' },
     { text1: 'Give in to', text2: 'Your passions' },
-  ]
-  const [heroCount, setHeroCount] = useState(2)
-  const [playStatus, setPlayStatus] = useState(false)
+  ];
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+  const interval =  setInterval(() => {
+      setHeroCount((count) => {return count === 2 ? 0 : count + 1})
+  }, 5000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div>
       <Background heroCount={heroCount} playStatus={playStatus} />
